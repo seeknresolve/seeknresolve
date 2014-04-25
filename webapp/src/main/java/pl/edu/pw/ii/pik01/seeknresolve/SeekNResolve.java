e1c2bf9509a2c3d5d949f9e35db52f8d4585e3d3
Rafał Nowak <rafal.nowak1@gmail.com>
2014-04-25 23:29:54 +0200

    Ugly fix for character encoding in html.

--- webapp/src/main/java/pl/edu/pw/ii/pik01/seeknresolve/SeekNResolve.java
+++ webapp/src/main/java/pl/edu/pw/ii/pik01/seeknresolve/SeekNResolve.java
@@ -2,14 +2,24 @@ package pl.edu.pw.ii.pik01.seeknresolve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class SeekNResolve {
+public class SeekNResolve implements EmbeddedServletContainerCustomizer {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SeekNResolve.class);
         app.setShowBanner(false);
         app.run(args);
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        mappings.add("html", "text/html;charset=utf-8");
+        container.setMimeMappings(mappings);
     }
 }
