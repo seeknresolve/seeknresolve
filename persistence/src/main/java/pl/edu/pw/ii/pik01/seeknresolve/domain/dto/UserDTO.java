18391de5bb3c3d87aee24e9689aa3493d0bcdc49
Mateusz Dziurdziak <dziurdziak.mateusz@gmail.com>
2014-04-26 16:10:45 +0200

    Added security skeleton

---
+++ persistence/src/main/java/pl/edu/pw/ii/pik01/seeknresolve/domain/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package pl.edu.pw.ii.pik01.seeknresolve.domain.dto;
+
+public class UserDTO {
+
+    private Long id;
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ service/src/main/java/pl/edu/pw/ii/pik01/seeknresolve/service/user/UserService.java
@@ -0,0 +1,65 @@
+package pl.edu.pw.ii.pik01.seeknresolve.service.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.edu.pw.ii.pik01.seeknresolve.domain.dto.UserDTO;
+import pl.edu.pw.ii.pik01.seeknresolve.domain.enitity.User;
+import pl.edu.pw.ii.pik01.seeknresolve.domain.repository.UserRepository;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public UserDTO createAndSaveNewUser(UserDTO userDTO){
+        User user = userRepository.save(crateUserFromUserDTO(userDTO));
+        return user == null ? null : buildDTOfromUser(user);
+    }
+
+    public UserDTO findOneById(Long id){
+        User user = userRepository.findOne(id);
+        if(user == null){
+            throw new EntityNotFoundException("User with id=" + id + " not found.");
+        }
+        return buildDTOfromUser(user);
+    }
+
+    public UserDTO findOneByLogin(String login){
+        User user = userRepository.findOneByLogin(login);
+        if(user == null){
+            throw new EntityNotFoundException("User with login=" + login + " not found.");
+        }
+        return buildDTOfromUser(user);
+    }
+
+    private User crateUserFromUserDTO(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setPassword(userDTO.getPassword());
+        user.setEmail(userDTO.getEmail());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setLogin(userDTO.getLogin());
+        //TODO: should initialize lists?
+        return user;
+    }
+
+    private UserDTO buildDTOfromUser(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setLogin(user.getLogin());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setPassword(user.getPassword());
+        return userDTO;
+
+    }
+}
