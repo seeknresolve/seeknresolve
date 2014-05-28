6f466d7b4a4f4753620989f8d3134777bf51c021
Rafał Nowak <rafal.nowak1@gmail.com>
2014-05-28 22:34:59 +0200

    Fix for project create in GUI.

--- webapp/src/main/webapp/js/BugControllers.js
+++ webapp/src/main/webapp/js/BugControllers.js
@@ -1,44 +1,44 @@
 var bugControllers = angular.module('bugControllers', []);
 
 bugControllers.controller('BugListController', ['$scope', '$http',
-    function($scope, $http) {
-        $scope.bugs = [ ];
+    function(scope, http) {
+        scope.bugs = [ ];
 
-        $http.get('/bug/all').success(function(data) {
-            $scope.bugs = data.object;
+        http.get('/bug/all').success(function(data) {
+            scope.bugs = data.object;
         }).error(function(data, status, headers, config) {
-            $scope.errorMessage = 'Can\'t fetch bugs list!';
+            scope.errorMessage = 'Can\'t fetch bugs list!';
         });
     }
 ]);
 
 bugControllers.controller('BugDetailsController', ['$scope', '$http', '$routeParams', '$location',
-    function($scope, $http, $routeParams, $location) {
-        $scope.tag = null;
+    function(scope, http, routeParams, location) {
+        scope.tag = null;
 
-        $http.get('/bug/' + $routeParams.tag).success(function(data) {
-            $scope.bug = data.object;
+        http.get('/bug/' + routeParams.tag).success(function(data) {
+            scope.bug = data.object;
         }).error(function(data, status, headers, config) {
             if(data.error) {
-                $scope.errorMessage = data.error;
+                scope.errorMessage = data.error;
             } else {
-                $scope.errorMessage = 'Can\'t fetch bug\'s details!';
+                scope.errorMessage = 'Can\'t fetch bug\'s details!';
             }
         });
 
-        $scope.deleteBug = function(tag) {
-            $http.delete('/bug/' + tag).success(function (data, status, headers, config) {
-                $location.path('/bug');
-                $scope.message = 'Bug ' + tag + ' was deleted';
+        scope.deleteBug = function(tag) {
+            http.delete('/bug/' + tag).success(function (data, status, headers, config) {
+                location.path('/bug');
+                scope.message = 'Bug ' + tag + ' was deleted';
             }).error(function (data, status, headers, config) {
-                $scope.errorMessage = 'Can\'t delete bug!';
+                scope.errorMessage = 'Can\'t delete bug!';
             });
         };
     }
 ]);
 
 bugControllers.controller('BugCreateController', ['$scope', '$http',
-    function($scope, $http) {
+    function(scope, http) {
 
     }
 ]);
--- webapp/src/main/webapp/js/ProjectControllers.js
+++ webapp/src/main/webapp/js/ProjectControllers.js
@@ -1,37 +1,50 @@
 var projectControllers = angular.module('projectControllers', []);
 
-projectControllers.controller('ProjectListController',
-        ['$scope', '$http',
-                function($scope, $http) {
-                    $scope.projects = [];
+projectControllers.controller('ProjectListController', ['$scope', '$http', '$rootScope',
+    function(scope, http, rootScope) {
+        scope.projects = [];
+        scope.message = rootScope.message;
 
-                    $http.get('/project/all').success(function(data) {
-                        $scope.projects = data.object;
-                    }).error(function(data, status, headers, config) {
-                        $scope.errorMessage = "Can't retrieve project list!";
-                    });
-                }
-        ]
-);
-
-projectControllers.controller('ProjectDetailsController', ['$scope', '$http', '$routeParams',
-    function($scope, $http, $routeParams) {
-        $scope.id = null;
-
-        $http.get('/project/' + $routeParams.id).success(function(data) {
-            $scope.project = data.object;
+        http.get('/project/all').success(function(data) {
+            scope.projects = data.object;
         }).error(function(data, status, headers, config) {
-            $scope.errorMessage = "Can't retrieve project details!";
+            scope.errorMessage = "Can't retrieve project list!";
         });
     }
 ]);
-bugControllers.controller('ProjectCreateController', ['$scope', '$http',
-    function($scope, $http) {
-        $http({
-            url:'/project/create',
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            data: {name: $scope.name, description: $scope.description}
+
+projectControllers.controller('ProjectDetailsController', ['$scope', '$http', '$routeParams',
+    function(scope, http, routeParams) {
+        scope.id = null;
+
+        http.get('/project/' + routeParams.id).success(function(data) {
+            scope.project = data.object;
+        }).error(function(data, status, headers, config) {
+            if(data.error) {
+                scope.errorMessage = data.error;
+            } else {
+                scope.errorMessage = "Can't retrieve project details!";
+            }
         });
+    }
+]);
+
+projectControllers.controller('ProjectCreateController', ['$scope', '$http', '$location', '$rootScope',
+    function(scope, http, location, rootScope) {
+        scope.createProject = function() {
+            var project = scope.project;
+            var params = JSON.stringify(project);
+
+            http.post('/project/create', params, {
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            }).success(function (data, status, headers, config) {
+                rootScope.message = 'Project created sucessfully';
+                location.path('/project');
+            }).error(function (data, status, headers, config) {
+                scope.errorMessage = "Creating project failed! " + data.error;
+            });
+        }
     }
 ]);
