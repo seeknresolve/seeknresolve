88b3d80d86dc02aece85156471e01f4214db9444
Rafał Nowak <rafal.nowak1@gmail.com>
2014-06-10 00:19:56 +0200

    Audit fix.

--- service/src/main/java/pl/edu/pw/ii/pik01/seeknresolve/audit/SpringSecurityAuditorAware.java
+++ service/src/main/java/pl/edu/pw/ii/pik01/seeknresolve/audit/SpringSecurityAuditorAware.java
@@ -1,10 +1,11 @@
-package pl.edu.pw.ii.pik01.seeknresolve.domain.audit;
+package pl.edu.pw.ii.pik01.seeknresolve.audit;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import pl.edu.pw.ii.pik01.seeknresolve.domain.entity.User;
+import pl.edu.pw.ii.pik01.seeknresolve.service.security.ContextUser;
 
 @Component("auditorAware")
 class SpringSecurityAuditorAware implements AuditorAware<User> {
@@ -14,6 +15,6 @@ class SpringSecurityAuditorAware implements AuditorAware<User> {
         if (authentication == null || !authentication.isAuthenticated()) {
             return null;
         }
-        return null;
+        return ((ContextUser)authentication.getPrincipal()).getUser();
     }
 }
