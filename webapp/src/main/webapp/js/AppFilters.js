dded6f2b709ee8fb0cf8266a3b81e241113b7312
Rafał Nowak <rafal.nowak1@gmail.com>
2014-06-10 22:32:49 +0200

    Bug state filter. #SNR-52

--- webapp/src/main/webapp/js/AppFilters.js
+++ webapp/src/main/webapp/js/AppFilters.js
@@ -5,3 +5,33 @@ appFilters.filter('capitalizeFilter', function() {
         return item.charAt(0) + item.substr(1).toLowerCase();
     };
 });
+
+appFilters.filter('bugStateFilter', function() {
+    return function(item) {
+        switch(item) {
+            case 'NEW':
+                return 'New';
+
+            case 'REJECTED':
+                return 'Rejected';
+
+            case 'IN_PROGRESS':
+                return 'In progress';
+
+            case 'READY_TO_TEST':
+                return 'Ready to test';
+
+            case 'REOPENED':
+                return 'Reopened';
+
+            case 'STOPPED':
+                return 'Stopped';
+
+            case 'CLOSED':
+                return 'Closed';
+                
+            default:
+                return item;
+        }
+    };
+});
